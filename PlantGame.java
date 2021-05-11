cdc2723350a5a2c856978495f1686580058ad3d6
Nathan <hosungyoum@gmail.com>
2021-05-11 19:05:10 +0900

    Pot

--- PlantGame.java
+++ PlantGame.java
@@ -1,8 +1,31 @@
-package PlacntGame;
+import java.util.Scanner; 
 
-public class Plant {
+public class plant {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner scan = new Scanner(System.in);
+
+        int money;
+        int size;
+        int height;
+        int dday;
+        int pot;
+
+        System.out.print("## 화분 선택하기 ##" + "\n[1] ? 화분 [2] $ 화분" + "\n## 화분를 선택해주세요:) ");
+        pot = scan.nextInt();
+
+        switch (pot) {
+        case 1:
+            System.out.print("단화한 ? 화분을선택하셨군요!!");
+            break;
+        case 2:
+            System.out.print("화려한 $ 화분을 선택하셨군요!!");
+            break;
+        default:
+            System.out.print("식물을 키우기 위해서는 화분은 필수!!");
+        }
+
+
     }
 }
 
